acd77a2b3c6f111df72a559b107373c23945163a
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-20 21:33:12 +0300

    Update App.jsx changed class to hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,106 +1,188 @@
 // import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import css from './App.module.css';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
-import { Component } from 'react';
-import css from './App.module.css';
 import { Loader } from 'components/Loader/Loader';
 import { Button } from 'components/Button/Button';
 import { Modal } from 'components/Modal/Modal';
 import { getImages } from '../../services/getImages';
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    loading: false,
-    error: null,
-    page: 1,
-    isOpenModal: false,
-    activeImage: '',
-    lastPage: true,
-  };
+export function App() {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [activeImage, setActiveImage] = useState('');
+  const [lastPage, setLastPage] = useState(true);
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.query;
-    const nextQuery = this.state.query;
-    const page = this.state.page;
-
-    if (prevQuery !== nextQuery || prevState.page !== page) {
-      this.setState({ loading: true, error: null });
-
-      getImages(nextQuery, page)
-        .then(res => {
-          if (res.status !== 200) {
-            return Promise.reject(new Error(`Oops, something went wrong...`));
-          } else return res.json();
-        })
-        .then(images => {
-          if (images.hits.length !== 0) {
-            return this.setState(prevState => ({
-              images: [...prevState.images, ...images.hits],
-              lastPage: page < Math.ceil(images.totalHits / 12),
-            }));
-          } else
-            return Promise.reject(
-              new Error(
-                `Sorry, there are no images matching your search query. Please try again`
-              )
-            );
-        })
-
-        .catch(error => this.setState({ error }))
-        .finally(() => this.setState({ loading: false }));
+  useEffect(() => {
+    if (!query) {
+      return;
     }
-  }
+    setLoading(true);
+    getImages(query, page)
+      .then(res => {
+        if (res.status !== 200) {
+          return Promise.reject(new Error(`Oops, something went wrong...`));
+        } else return res.json();
+      })
+      .then(data => {
+        if (data.hits.length !== 0) {
+          return (
+            setImages(images => [...images, ...data.hits]),
+            setLastPage(page < Math.ceil(data.totalHits / 12))
+          );
+        } else
+          return Promise.reject(
+            new Error(
+              `Sorry, there are no images matching your search query. Please try again`
+            )
+          );
+      })
 
-  handleButtonClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+      .catch(error => setError(error))
+      .finally(() => setLoading(false));
+  }, [query, page]);
+
+  const handleButtonClick = () => {
+    setPage(page => page + 1);
   };
 
-  handleImageClick = activeImage => {
-    this.setState({ activeImage, isOpenModal: true });
+  const handleImageClick = activeImage => {
+    setActiveImage(activeImage);
+    setIsOpenModal(true);
   };
 
-  handelImageMogalClose = () => {
-    this.setState({ activeImage: '', isOpenModal: false });
+  const handelImageModalClose = () => {
+    setActiveImage('');
+    setIsOpenModal(false);
   };
 
-  handleFormSubmit = query => {
-    this.setState({
-      query,
-      images: [],
-      loading: false,
-      error: null,
-      page: 1,
-      isOpenModal: false,
-      activeImage: '',
-    });
+  const handleFormSubmit = query => {
+    setQuery(query);
+    setImages([]);
+    setLoading(false);
+    setError(null);
+    setPage(1);
+    setIsOpenModal(false);
+    setActiveImage('');
   };
 
-  render() {
-    const { error, loading, images, isOpenModal, activeImage, lastPage } =
-      this.state;
-    return (
-      <div className={css.app}>
-        <Searchbar onSubmit={this.handleFormSubmit} />
+  return (
+    <div className={css.app}>
+      <Searchbar onSubmit={handleFormSubmit} />
 
-        {error && <h1>{error.massege}</h1>}
-        {isOpenModal && (
-          <Modal
-            image={activeImage}
-            onCloseModal={this.handelImageMogalClose}
-          />
-        )}
+      {error && <h1>{error.message}</h1>}
+      {isOpenModal && (
+        <Modal image={activeImage} onCloseModal={handelImageModalClose} />
+      )}
 
-        <ImageGallery images={images} onImageClick={this.handleImageClick} />
-        {loading && <Loader />}
+      <ImageGallery images={images} onImageClick={handleImageClick} />
+      {loading && <Loader />}
 
-        {images.length !== 0 && lastPage && (
-          <Button onClick={this.handleButtonClick}>Load More</Button>
-        )}
-      </div>
-    );
-  }
+      {images.length !== 0 && lastPage && (
+        <Button onClick={handleButtonClick}>Load More</Button>
+      )}
+    </div>
+  );
 }
+
+// export class App extends Component {
+//   state = {
+//     query: '',
+//     images: [],
+//     loading: false,
+//     error: null,
+//     page: 1,
+//     isOpenModal: false,
+//     activeImage: '',
+//     lastPage: true,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const prevQuery = prevState.query;
+//     const nextQuery = this.state.query;
+//     const page = this.state.page;
+
+//     if (prevQuery !== nextQuery || prevState.page !== page) {
+//       this.setState({ loading: true, error: null });
+
+//       getImages(nextQuery, page)
+//         .then(res => {
+//           if (res.status !== 200) {
+//             return Promise.reject(new Error(`Oops, something went wrong...`));
+//           } else return res.json();
+//         })
+//         .then(images => {
+//           if (images.hits.length !== 0) {
+//             return this.setState(prevState => ({
+//               images: [...prevState.images, ...images.hits],
+//               lastPage: page < Math.ceil(images.totalHits / 12),
+//             }));
+//           } else
+//             return Promise.reject(
+//               new Error(
+//                 `Sorry, there are no images matching your search query. Please try again`
+//               )
+//             );
+//         })
+
+//         .catch(error => this.setState({ error }))
+//         .finally(() => this.setState({ loading: false }));
+//     }
+//   }
+
+//   handleButtonClick = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   handleImageClick = activeImage => {
+//     this.setState({ activeImage, isOpenModal: true });
+//   };
+
+//   handelImageMogalClose = () => {
+//     this.setState({ activeImage: '', isOpenModal: false });
+//   };
+
+//   handleFormSubmit = query => {
+//     this.setState({
+//       query,
+//       images: [],
+//       loading: false,
+//       error: null,
+//       page: 1,
+//       isOpenModal: false,
+//       activeImage: '',
+//     });
+//   };
+
+//   render() {
+//     const { error, loading, images, isOpenModal, activeImage, lastPage } =
+//       this.state;
+//     return (
+//       <div className={css.app}>
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+
+//         {error && <h1>{error.massege}</h1>}
+//         {isOpenModal && (
+//           <Modal
+//             image={activeImage}
+//             onCloseModal={this.handelImageMogalClose}
+//           />
+//         )}
+
+//         <ImageGallery images={images} onImageClick={this.handleImageClick} />
+//         {loading && <Loader />}
+
+//         {images.length !== 0 && lastPage && (
+//           <Button onClick={this.handleButtonClick}>Load More</Button>
+//         )}
+//       </div>
+//     );
+//   }
+// }
