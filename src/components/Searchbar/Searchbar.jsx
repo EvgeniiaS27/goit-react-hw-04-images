62382e51d43000d6586979656aa02c20d4a12d49
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-20 21:34:07 +0300

    Update Searchbar.jsx changed class to hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,43 +1,79 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import css from './Searchbar.module.css';
 import { ReactComponent as SearchIcon } from '../../icons/search.svg';
 
-export class Searchbar extends Component {
-  state = {
-    query: '',
+export function Searchbar({ onSubmit }) {
+  const [query, setQuery] = useState('');
+
+  const handleChangeInput = e => {
+    setQuery(e.currentTarget.value.toLowerCase());
   };
 
-  handleChangeInput = e => {
-    this.setState({ query: e.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmitForm = e => {
+  const handleSubmitForm = e => {
     e.preventDefault();
-    if (this.state.query.trim() === '') {
+    if (query.trim() === '') {
       alert('Введите запрос в поле поиска');
     }
 
-    this.props.onSubmit(this.state.query);
-    this.setState({ query: '' });
+    onSubmit(query);
+    setQuery('');
   };
 
-  render() {
-    return (
-      <header className={css.searchbar}>
-        <form className={css.form} onSubmit={this.handleSubmitForm}>
-          <button type="submit" className={css.formButton}>
-            <SearchIcon fill="grey" />
-          </button>
+  return (
+    <header className={css.searchbar}>
+      <form className={css.form} onSubmit={handleSubmitForm}>
+        <button type="submit" className={css.formButton}>
+          <SearchIcon fill="grey" />
+        </button>
 
-          <input
-            className={css.input}
-            type="text"
-            value={this.state.query}
-            onChange={this.handleChangeInput}
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          className={css.input}
+          type="text"
+          value={query}
+          onChange={handleChangeInput}
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
 }
+
+// export class Searchbar extends Component {
+//   state = {
+//     query: '',
+//   };
+
+//   handleChangeInput = e => {
+//     this.setState({ query: e.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmitForm = e => {
+//     e.preventDefault();
+//     if (this.state.query.trim() === '') {
+//       alert('Введите запрос в поле поиска');
+//     }
+
+//     this.props.onSubmit(this.state.query);
+//     this.setState({ query: '' });
+//   };
+
+//   render() {
+//     return (
+//       <header className={css.searchbar}>
+//         <form className={css.form} onSubmit={this.handleSubmitForm}>
+//           <button type="submit" className={css.formButton}>
+//             <SearchIcon fill="grey" />
+//           </button>
+
+//           <input
+//             className={css.input}
+//             type="text"
+//             value={this.state.query}
+//             onChange={this.handleChangeInput}
+//             placeholder="Search images and photos"
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
